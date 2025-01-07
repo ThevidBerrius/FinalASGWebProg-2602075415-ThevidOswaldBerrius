f0d11a9ab239201fbd14b4d3de935b1d7901e99b
Thevid Oswald Berrius <thevidob@gmail.com>
2025-01-07 18:20:27 +0700

    Fix: Wrong Commit File

--- resources/views/pages/friends.blade.php
+++ resources/views/pages/friends.blade.php
@@ -2,48 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">@lang('lang.payment')</div>
-                <div class="card-body">
-                    <h1>@lang('lang.your_friend')</h1>
-                    <p>@lang('lang.registration_fee'): Rp{{ number_format($price) }}</p>
-
-                    @if(session('overpaid'))
-                        <p>@lang('lang.overpaid_message', ['amount' => number_format(session('overpaid_amount'))])</p>
-                        <form method="POST" action="{{ route('auth.payment.process') }}">
-                            @csrf
-                            <button type="submit" name="balance" value="yes" class="btn btn-success">
-                                @lang('lang.enter_wallet_balance')
-                            </button>
-                            <button type="submit" name="balance" value="no" class="btn btn-danger">
-                                @lang('lang.reenter_payment_amount')
-                            </button>
-                        </form>
-                    @else
-                        <form method="POST" action="{{ route('auth.payment.process') }}">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="payment_amount" class="form-label">@lang('lang.payment_amount')</label>
-                                <input type="number" name="payment_amount" id="payment_amount" class="form-control" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary">@lang('lang.submit_payment')</button>
-                        </form>
-                    @endif
-
-                    @if($errors->any())
-                        <div class="alert alert-danger mt-3">
-                            <ul>
-                                @foreach($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+    <h1>@lang('lang.your_friend')</h1>
+    <div class="row">
+        @foreach($friends as $friend)
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="{{ optional($friend->avatar)->image_data ?: 'default-avatar.png' }}" class="card-img-top" alt="@lang('lang.avatar')">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $friend->name }}</h5>
+                        <p class="card-text">{{ $friend->occupation->name }}</p>
+                        <a href="{{ route('messages.show', ['friendId' => $friend->id]) }}" class="btn btn-primary">@lang('lang.chat')</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
